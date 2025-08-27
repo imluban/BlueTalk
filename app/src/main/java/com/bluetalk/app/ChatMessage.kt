aebaaa89bd3a9be7a76b54064c5299a15da2c07a
Luban Noor <lubanjrofficial@gmail.com>
2025-08-27 20:29:22 +0600

    update: fixed the hell out of it

--- app/src/main/java/com/bluetalk/app/ChatMessage.kt
+++ app/src/main/java/com/bluetalk/app/ChatMessage.kt
@@ -1,6 +1,7 @@
 package com.bluetalk.app
 
 data class ChatMessage(
+    val nick: String,
     val text: String,
     val isIncoming: Boolean = true,
     val timestamp: Long = System.currentTimeMillis()
